a376d26c91e2b77ef3bd8dc59580a1ca7c92ff5a
gudata <i.bardarov@gmail.com>
2024-01-19 13:47:57 +0200

    new version

--- list.py
+++ list.py
@@ -1,25 +1,30 @@
 import concurrent.futures
 import sys
+
 import boto3
 import click
 
 
 def list_objects(bucket_name, prefix):
-    s3 = boto3.client('s3')
-    paginator = s3.get_paginator('list_objects_v2')
+    s3 = boto3.client("s3")
+    paginator = s3.get_paginator("list_objects_v2")
     page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
     for page in page_iterator:
-        if 'Contents' in page:
-            for obj in page['Contents']:
+        if "Contents" in page:
+            for obj in page["Contents"]:
                 yield obj
 
 
-
-
 @click.command()
-@click.argument('bucket_name')
-@click.option('--prefix', default="", help="No slash at start, or empty")
-@click.option('--storage_class', "-c", default=[], multiple=True, help='List only specific class of objects (GLACIER, STANDARD, STANDARD_IA)')
+@click.argument("bucket_name")
+@click.option("--prefix", default="", help="No slash at start, or empty")
+@click.option(
+    "--storage_class",
+    "-c",
+    default=[],
+    multiple=True,
+    help="List only specific class of objects (GLACIER, STANDARD, STANDARD_IA)",
+)
 def print_objects(bucket_name, prefix, storage_class):
     """
     list bucket_name prefix
@@ -42,11 +47,11 @@ def print_objects(bucket_name, prefix, storage_class):
     print(f"Working on {bucket_name} prefix: {prefix}")
     for obj in list_objects(bucket_name, prefix):
         if show_all:
-            print(obj['Key'])
+            print(obj["Key"])
         else:
-            if obj['StorageClass'] in storage_class:
-                print(obj['Key'])
+            if obj["StorageClass"] in storage_class:
+                print(obj["Key"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print_objects()
